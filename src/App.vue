80ee4f00a1734a2d43587f1d306cf8908876802b
castordd <castord.d@gmail.com>
2019-06-23 17:48:40 -0300

    uso de slots

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <h1>Vue components</h1>
     <Post>
-      <h2>Components no Vue</h2>
-      <p>Componets são uma das peças mais importantes no Vue</p>
+      <h2 slot="cabecalho">Components no Vue</h2>
+      <template>
+        <p class="paragrafo">Components são uma das peças mais importantes no Vue</p>
+        <span>...</span>
+      </template>
+      <small slot="rodape">por {{ autor }}</small>
     </Post>
   </div>
 </template>
@@ -14,6 +18,21 @@ import Post from './components/Post.vue'
 export default {
   components: {
     Post
-  }  
+  },
+  data(){
+    return {
+      autor: 'Castor Dioscurois'
+    }
+  }
 }
 </script>
+<style lang="scss" scoped>
+  .container {
+    max-width: 960px;
+    margin: auto;
+    padding:0 18px;
+  }
+
+  
+</style>
+
--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,7 +1,43 @@
 <template>
-  <div>
-    <slot></slot>
+  <div class="post">
+    <div class="post-cabecalho">
+      <slot name="cabecalho"></slot>
+    </div>
+    <div class="post-conteudo">
+      <slot ></slot>
+    </div>
+    <div class="post-rodape">
+      <slot name="rodape"></slot>
+      <a href="" class="link">
+        <slot name="botao">Ler mais</slot>
+      </a>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  .post {
+    margin: 15px auto;
+
+    &-cabecalho {
+      padding: 5px 12px;
+      background-color: #eee;
+    }
+    &-conteudo {
+      font-size: 16px;
+    }
+    &-rodape {
+      font-style: italic;
+    }
+    .link {
+      float: right;
+    }
+
+    .paragrafo {
+      color: green;
+    }
+  }
+</style>
+
 
 
